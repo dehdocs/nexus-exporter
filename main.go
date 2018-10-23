976574e435b7db30a28babbb33a4ffac811d60f4
André Luis da Silva <andre.bueno@accenture.com>
2018-10-23 16:08:14 -0200

    Iniciando Projeto

--- main.go
+++ main.go
@@ -16,14 +16,14 @@ func main() {
 
 	var (
 		version			= "1.0.0.dev"
-		listen			= flag.String("web.listen-address",":8080", "Addressto listen")
-		metricPath		= flag.String("web.telemetry-path","/metrics","Path of the metrics")
-		landingPage		= []byte("<html><head><title>Nexus-Exporter</title></head><h1>NEXUS EXPORTER "+version+"</h1>")
+		//listen			= flag.String("web.listen-address",":8080", "Addressto listen")
+		//metricPath		= flag.String("web.telemetry-path","/metrics","Path of the metrics")
+		//landingPage		= []byte("<html><head><title>Nexus-Exporter</title></head><h1>NEXUS EXPORTER "+version+"</h1>")
 		nUrl			= flag.String("nexus.uri", "http://10.129.176.139:8081", "HTTP API address of nexus.")
 		nPath			= flag.String("nexus.path", "/service/siesta/atlas/system-information", "nexus api path.")
 		nUser			= flag.String("nexus.user", "admin", "nexus password.")
 		nPass			= flag.String("nexus.pass", "admin123", "nexus password.")
-		data 			string
+		//data 			string
 	)
 
 	flag.Parse()
