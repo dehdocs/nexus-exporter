0ee7ee128e10f7523c062ed6012343e1fd6a6c95
André Luis da Silva <andre.bueno@accenture.com>
2018-10-23 12:39:58 -0200

    fix errors

--- main.go
+++ main.go
@@ -46,66 +46,15 @@ func NewExporter(nexusUrl string, nexusPath string) *Exporter {
 }
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	e.scrape(ch)
-	ch <- e.duration
-	ch <- e.totalScrapes
-	ch <- e.error
-	e.scrapeErrors.Collect(ch)
-	ch <- e.up
+	ch <- e.availableProcessors
 }
 
 func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 	e.totalScrapes.Inc()
 	var err error
 	defer func(begun time.Time) {
-		e.duration.Set(time.Since(begun).Seconds())
-		if err == nil {
-			e.error.Set(0)
-		} else {
-			e.error.Set(1)
-		}
+		e.availableProcessors.Set(time.Since(begun).Seconds())
 	}(time.Now())
-
-	db, err := sql.Open("oci8", e.dsn)
-	if err != nil {
-		log.Errorln("Error opening connection to database:", err)
-		return
-	}
-	defer db.Close()
-
-	isUpRows, err := db.Query("SELECT 1 FROM DUAL")
-	if err != nil {
-		log.Errorln("Error pinging oracle:", err)
-		e.up.Set(0)
-		return
-	}
-	isUpRows.Close()
-	e.up.Set(1)
-
-	if err = ScrapeActivity(db, ch); err != nil {
-		log.Errorln("Error scraping for activity:", err)
-		e.scrapeErrors.WithLabelValues("activity").Inc()
-	}
-
-	if err = ScrapeTablespace(db, ch); err != nil {
-		log.Errorln("Error scraping for tablespace:", err)
-		e.scrapeErrors.WithLabelValues("tablespace").Inc()
-	}
-
-	if err = ScrapeWaitTime(db, ch); err != nil {
-		log.Errorln("Error scraping for wait_time:", err)
-		e.scrapeErrors.WithLabelValues("wait_time").Inc()
-	}
-
-	if err = ScrapeSessions(db, ch); err != nil {
-		log.Errorln("Error scraping for sessions:", err)
-		e.scrapeErrors.WithLabelValues("sessions").Inc()
-	}
-
-	if err = ScrapeProcesses(db, ch); err != nil {
-		log.Errorln("Error scraping for process:", err)
-		e.scrapeErrors.WithLabelValues("process").Inc()
-	}
-
 }
 
 func main() {
