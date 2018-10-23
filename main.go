28dd8882729cf685494559649ad0c02d4481182c
André Luis da Silva <andre.bueno@accenture.com>
2018-10-23 15:49:50 -0200

    Iniciando Projeto

--- main.go
+++ main.go
@@ -11,100 +11,48 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 )
-
-var (
-	version		= "1.0.0.dev"
-	listen		= flag.String("web.listen-address",":8080", "Addressto listen")
-	metricPath	= flag.String("web.telemetry-path","/metrics","Path of the metrics")
-	landingPage	= []byte("<html><head><title>Nexus-Exporter</title></head><h1>NEXUS EXPORTER "+version+"</h1>")
-)
-
-const (
-	namespace	= "nexus"
-	exporter	= "exporter"
-)
-
-type Exporter struct {
-	nexusUrl				string
-	nexusPath				string
-	nexusUser				string
-	nexusPass				string
-	auth					string
-	availableProcessors		prometheus.Gauge
-	//freeMemory				prometheus.Gauge
-	//totalMemory				prometheus.Gauge
-	//maxMemory				prometheus.Gauge
-	//threads					prometheus.Gauge
-}
-func NewExporter(nexusUrl string, nexusPath string) *Exporter {
-	return &Exporter{
-		nexusUrl: nexusUrl,
-		nexusPath: nexusPath,
-		availableProcessors: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Subsystem: exporter,
-			Name:      "nexus_processors_available",
-			Help:      "Quantity of processors are available in nexus.",
-		}),
-	}
-}
-func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	e.scrape(ch)
-	ch <- e.availableProcessors
-}
-
-func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
-	//var err error
-	defer func(begun time.Time) {
-		e.availableProcessors.Set(time.Since(begun).Seconds())
-	}(time.Now())
-}
-func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	metricCh := make(chan prometheus.Metric)
-	doneCh := make(chan struct{})
-
-	go func() {
-		for m := range metricCh {
-			ch <- m.Desc()
-		}
-		close(doneCh)
-	}()
-
-	e.Collect(metricCh)
-	close(metricCh)
-	<-doneCh
-
-}
-
 func main() {
-	flag.Parse()
-	log.Infoln("Starting nexus_exporter " + version)
 	nexusUrl := os.Getenv("NEXUS_URL")
 	nexusPath := os.Getenv("NEXUS_PATH")
 	nexusUser := os.Getenv("NEXUS_USER")
 	nexusPass := os.Getenv("NEXUS_PASS")
 
-	log.Infoln(nexusUrl+nexusPath)
-	client := &http.Client{}
-	req, err:= http.NewRequest("GET", nexusUrl+nexusPath, nil)
-	req.SetBasicAuth(nexusUser, nexusPass)
-	resp, err := client.Do(req)
+	var (
+		version		= "1.0.0.dev"
+		listen		= flag.String("web.listen-address",":8080", "Addressto listen")
+		metricPath	= flag.String("web.telemetry-path","/metrics","Path of the metrics")
+		landingPage	= []byte("<html><head><title>Nexus-Exporter</title></head><h1>NEXUS EXPORTER "+version+"</h1>")
+	)
 
-	if err != nil{
-        log.Fatal(err)
+	flag.Parse()
+
+	nexusUrl, ok := os.LookupEnv("NEXUS_URL")
+	if ok {
+		*nexusUrl = nexusUrl
 	}
-	bodyText, err := ioutil.ReadAll(resp.Body)
-    s := string(bodyText)
-	log.Infoln(s)
-	
-	
-	exporter := NewExporter(nexusUrl, nexusPath)
-	
-	prometheus.MustRegister(exporter)
+	nexusPath, ok := os.LookupEnv("NEXUS_URL")
+	if ok {
+		*nexusPath = nexusPath
+	}
+	nexusUser, ok := os.LookupEnv("NEXUS_URL")
+	if ok {
+		*nexusUser = nexusUser
+	}
+	nexusPass, ok := os.LookupEnv("NEXUS_URL")
+	if ok {
+		*nexusPass = nexusPass
+	}
+
+	data = getMetrics();
+
 	http.Handle(*metricPath, prometheus.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(landingPage)
 	})
 	log.Infoln("Listening on", *listen)
 	log.Fatal(http.ListenAndServe(*listen, nil))
+}
+
+func getMetrics(){
+	log.Infoln(*nexusPass)
 }
